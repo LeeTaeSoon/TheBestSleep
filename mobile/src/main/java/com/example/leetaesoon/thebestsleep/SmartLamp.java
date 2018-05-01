cce5aea74fb6ed28228470de837f82a42e401a15
DESKTOP-23NVG7J\wkddu <wkddus119@naver.com>
2018-05-01 22:16:04 +0900

    Main화면 버튼 추가

---
+++ mobile/src/main/java/com/example/leetaesoon/thebestsleep/SmartLamp.java
@@ -0,0 +1,13 @@
+package com.example.leetaesoon.thebestsleep;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SmartLamp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_smart_lamp);
+    }
+}
