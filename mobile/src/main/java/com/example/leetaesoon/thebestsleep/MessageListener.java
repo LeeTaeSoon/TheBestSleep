87f022434f95ad5de73a95fb21cc2004d5d746dc
LeeTaeSoon <nrst136@naver.com>
2018-05-15 21:22:54 +0900

    워치 -> 모바일 메시지 전송
    Listener 를 사용한 메시지 전송
    
    Looper.loop() 를 통해 모바일에서 Thread 가 죽지 않도록 처리
    
    Listener 사용을 위해 menifest 에 service 클래스 추가
    - BIND_LISTENER 대신 MESSAGE_RECEIVED 사용
    - data 를 추가해야 정상적으로 메시지 수신

---
+++ mobile/src/main/java/com/example/leetaesoon/thebestsleep/MessageListener.java
@@ -0,0 +1,24 @@
+package com.example.leetaesoon.thebestsleep;
+
+import android.os.Looper;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.WearableListenerService;
+
+public class MessageListener extends WearableListenerService {
+    @Override
+    public void onMessageReceived(final MessageEvent messageEvent) {
+        String data = new String(messageEvent.getData());
+        showToast(data);
+        Log.d("MessageListener", "message : " + data);
+        super.onMessageReceived(messageEvent);
+
+        Looper.loop();
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+}
