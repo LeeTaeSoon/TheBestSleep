db95df23085275bc33dfa52d7f43f530567fc60a
LeeTaeSoon <nrst136@naver.com>
2018-05-20 20:30:31 +0900

    서비스 한번만 실행 & 죽지않음
    running 변수를 사용해 서비스가 돌고 있다면 더 만들지 않음
    
    foreground 를 사용해 서비스가 죽지않도록 함

--- wear/src/main/java/com/example/leetaesoon/thebestsleep/SensorService.java
+++ wear/src/main/java/com/example/leetaesoon/thebestsleep/SensorService.java
@@ -1,4 +1,6 @@
 package com.example.leetaesoon.thebestsleep;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -10,6 +12,7 @@ public class SensorService extends Service {
     private static final String TAG = "SensorService";
     SensorHandler sensorHandler;
     Context context = this;
+    boolean running = false;
 
     public SensorService() {
         super();
@@ -18,13 +21,19 @@ public class SensorService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.d(TAG, "onCreate()");
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        sensorHandler.stopMeasure();
-        Log.d(TAG, "Called sensorHandler.stopMeasure()");
+        Log.d(TAG, "onDestroy()");
+        running = false;
+
+        if (sensorHandler != null) {
+            sensorHandler.stopMeasure();
+            Log.d(TAG, "Called sensorHandler.stopMeasure()");
+        }
     }
 
     @Override
@@ -34,21 +43,39 @@ public class SensorService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "Enter onStartCommand");
         // TODO : 현재 여러 번 실행이 될 경우 계속해서 새로운 쓰레드를 만들어 서비스를 돌려서 여러 개가 생김
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                Log.d(TAG, "Enter onStartCommand");
-                Log.d(TAG, "Create sensorHandler object");
-                sensorHandler = new SensorHandler(context);
+        if (running) {
+            Log.d(TAG, "Service is already running");
+            return  START_NOT_STICKY;
+        } else {
+            running = true;
 
-                sensorHandler.startMeasure();
-                Log.d(TAG, "Called sensorHandler.startMeasure()");
+            startForeground(1, new Notification());
+            NotificationManager nm = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+            Notification notification;
+            notification = new Notification.Builder(getApplicationContext())
+                    .setContentTitle("")
+                    .setContentText("")
+                    .setSmallIcon(getApplicationInfo().icon)
+                    .build();
 
-                Log.d(TAG, "Exit onStartCommand");
-            }
-        }).start();
+            nm.notify(startId, notification);
+            nm.cancel(startId);
 
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    Log.d(TAG, "Create sensorHandler object");
+                    sensorHandler = new SensorHandler(context);
+
+                    sensorHandler.startMeasure();
+                    Log.d(TAG, "Called sensorHandler.startMeasure()");
+                }
+            }).start();
+        }
+
+        Log.d(TAG, "Exit onStartCommand");
         return super.onStartCommand(intent, flags, startId);
     }
 }
