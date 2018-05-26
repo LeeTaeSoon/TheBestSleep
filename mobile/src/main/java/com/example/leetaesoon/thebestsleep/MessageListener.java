c2fe8a8cb2e5fdbc2ca1c2ec5020b8975775d117
LeeTaeSoon <nrst136@naver.com>
2018-05-26 16:42:20 +0900

    센서 값 받을때 DB 에도 저장
    나중을 위해 파일에는 그대로 저장하면서 내부 DB 에 저장해서 사용

--- mobile/src/main/java/com/example/leetaesoon/thebestsleep/MessageListener.java
+++ mobile/src/main/java/com/example/leetaesoon/thebestsleep/MessageListener.java
@@ -7,6 +7,8 @@ import android.widget.Toast;
 import com.google.android.gms.wearable.MessageEvent;
 import com.google.android.gms.wearable.WearableListenerService;
 
+import java.util.ArrayList;
+
 public class MessageListener extends WearableListenerService {
     @Override
     public void onMessageReceived(final MessageEvent messageEvent) {
@@ -14,6 +16,45 @@ public class MessageListener extends WearableListenerService {
         //showToast(data);
         ExternalStorageHandler externalStorageHandler = new ExternalStorageHandler();
         externalStorageHandler.writeFile(data + "\n");
+
+        DBHandler dbHandler = new DBHandler(getApplicationContext(), DBHandler.DATABASE_NAME, null, 1);
+        ArrayList<HeartRate> heartRates = new ArrayList<>();
+        ArrayList<Acceleration> accelerometers = new ArrayList<>();
+
+        String[] datas = data.split("\n");
+
+        for (int i = 0; i < datas.length; i++) {
+            //Log.d("MessageListener", "datas[" + i + "] : " + datas[i]);
+            String[] arr = datas[i].split(" ");
+
+            if(arr.length > 1) {
+                long time = Long.parseLong(arr[0]);
+                //Log.d("MessageListener", "time : " + time);
+                //Log.d("MessageListener", "type : " + arr[1]);
+
+                switch (arr[1]) {
+                    case "HeartRate":
+                        int rate = Integer.parseInt(arr[3]);
+                        //Log.d("MessageListener", "heart rate : " + rate);
+                        HeartRate heartRate = new HeartRate(rate, time);
+                        heartRates.add(heartRate);
+                        break;
+
+                    case "Accelerometer":
+                        float x = Float.parseFloat(arr[3]);
+                        float y = Float.parseFloat(arr[4]);
+                        float z = Float.parseFloat(arr[5]);
+                        //Log.d("MessageListener", "accelerometer : " + x + " " + y + " " + z);
+                        Acceleration acc = new Acceleration(time, x, y, z);
+                        accelerometers.add(acc);
+                        break;
+                }
+            }
+        }
+
+        //Log.d("MessageListener", "Start save datas in db");
+        dbHandler.addAllHeartRate(heartRates);
+        dbHandler.addAllAcceleration(accelerometers);
         //Log.d("MessageListener", "message : " + data);
         super.onMessageReceived(messageEvent);
 
